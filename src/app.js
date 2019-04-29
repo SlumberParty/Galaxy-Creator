72eb0f1fdb74bb82018654e613e9a8e2df06ede4
Claire Cassidy <clacassidy@gmail.com>
2019-04-28 20:06:51 -0700

    fixed single semicolin error over three hours

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ import makePlanet from './make-planet.js';
 const planetButtons = document.querySelectorAll('.planet');
 const sunImage = document.getElementById('sun');
 
-for(let i = 0; i < planetButtons.length; i++); {
+for(let i = 0; i < planetButtons.length; i++) {
     const planetMaker = planetButtons[i];
-
+    
     planetMaker.addEventListener('click', () => {
         makePlanet(sunImage, planetMaker.value);
+        console.log('in event listener');
     });
 
 }
